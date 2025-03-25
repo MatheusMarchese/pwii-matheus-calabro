72508ee9913036746d732c05eb849c9047859fdd
Matheus Marchese <matmcalabro@gmail.com>
2025-03-25 16:31:29 -0300

    Aula sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+    ECHO "Hello Word!<br>";
+    echo "Hello Word!<br>";
+    EcHo "Hello Word!<br>";
+
+    $nome = "Matheus";
+    $sobrenome = "Marhcese";
+    $numero = 29;
+
+    echo $nome . "lindo" . "<br>";
+     echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero  . "<p>" . $nome .
+    " " . $sobrenome . "</p>";
+?>
